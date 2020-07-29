b7719511ce9e431f5c0c8b266b659fb7d31e367a
World Wide <agwalker249@gmail.com>
2020-07-28 19:38:42 -0500

    Added gh-pages and deploy configs to package.json

---
+++ client/build/precache-manifest.0c55ca9392c518e3f1a26a9e5924f06b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321cb4340ea459eaaeb1904acafbab0d",
+    "url": "/job-map/index.html"
+  },
+  {
+    "revision": "24330d22bfb1daf1ba02",
+    "url": "/job-map/static/css/main.6f304b5b.chunk.css"
+  },
+  {
+    "revision": "7e4215cf8e052a7b4954",
+    "url": "/job-map/static/js/2.bac1adab.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/job-map/static/js/2.bac1adab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24330d22bfb1daf1ba02",
+    "url": "/job-map/static/js/main.2943c73c.chunk.js"
+  },
+  {
+    "revision": "e5c6710d15456d735add",
+    "url": "/job-map/static/js/runtime-main.b078931d.js"
+  },
+  {
+    "revision": "1cf290d08007c7ba71e4d07516a6b307",
+    "url": "/job-map/static/media/austin-tx-map.1cf290d0.PNG"
+  }
+]);
