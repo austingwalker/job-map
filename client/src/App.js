f6011c9a7853f9326821a79cd47067974cdf49b8
World Wide <agwalker249@gmail.com>
2020-07-28 20:00:31 -0500

    Removed BrowserRouter from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home";
 import './App.css';
 
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/home" component={Home} />
-        {/* <Route component={NoMatch} /> */}
-      </Switch>
-  </Router> 
-  );
-}
+const App = () => <Home/>;
 
 export default App;
